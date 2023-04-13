5df096ba163869a901702358ced8408b183cf791
Carmen Lee <carmenlee2278@gmail.com>
2023-04-13 15:03:35 -0700

    rearranged order of middleware

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -8,17 +8,6 @@ const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
-const validateLogin = [
-  check('credential')
-    .exists({ checkFalsy: true })
-    .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
-  handleValidationErrors
-];
-
 const router = express.Router();
 
 router.post(
@@ -84,6 +73,17 @@ router.get(
       } else return res.json({ user: null });
     }
   );
+
+  const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
 
 
 router.post(
