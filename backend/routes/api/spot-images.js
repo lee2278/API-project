e695d45ab1c55d4ec95f30369fa7ea460bdc1c84
Carmen Lee <carmenlee2278@gmail.com>
2023-04-21 09:02:03 -0700

    made spot-images-js file

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { SpotImage} = require('../../db/models');
+const { requireAuth } = require('../../utils/auth')
+
+
+
+
+
+
+module.exports = router;
