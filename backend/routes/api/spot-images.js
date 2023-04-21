1f15117c4c3b6a0c63cc9739cfd67b419893a711
Carmen Lee <carmenlee2278@gmail.com>
2023-04-21 09:15:57 -0700

    deleting spot image appears to work

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -4,7 +4,32 @@ const { SpotImage} = require('../../db/models');
 const { requireAuth } = require('../../utils/auth')
 
 
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const paramsId = parseInt(req.params.imageId)
+    const { user } = req;
+    const imageToDelete = await SpotImage.findByPk(paramsId)
 
+    if (!imageToDelete) {
+        return res.status(404).json({
+            message: "Spot Image couldn't be found"
+        })
+    }
+
+    const spot = await imageToDelete.getSpot()
+
+    if (user.id !== spot.ownerId) {
+        return res.status(403).json({
+            message: "Forbidden"
+        })
+    }
+
+    await imageToDelete.destroy()
+
+    return res.status(200).json({
+        message: "Successfully deleted"
+    })
+
+})
 
 
 
