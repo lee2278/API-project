cdf3a0cc6da370d2579c81e48940c0af635413e2
Carmen Lee <carmenlee2278@gmail.com>
2023-04-20 21:43:27 -0700

    added basic structure of get bookings

--- backend/db/seeders/20230417184012-demo-bookings.js
+++ backend/db/seeders/20230417184012-demo-bookings.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       {
         spotId: 3,
-        userId: 1,
+        userId: 2,
         startDate: '2023-03-01',
         endDate: '2023-03-10'
       }
---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const { Booking } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth')
+
+router.get('/current', requireAuth, async (req, res) => {
+
+    const { user } = req;
+
+    const userBookings = await Booking.findAll({
+        where: {
+            userId: user.id
+        }
+    })
+
+    let listOfBookings = [];
+
+    for (let i = 0; i < userBookings.length; i++) {
+        let booking = userBookings[i].toJSON();
+        let user = await userBookings[i].getUser({
+            attributes: ['id', 'firstName', 'lastName']
+        });
+        let spot = await userBookings[i].getSpot({attributes: {exclude: ['createdAt', 'updatedAt', 'description']}})
+        let spotJson = spot.toJSON()
+        
+
+        
+        let spotImages = await spot.getSpotImages()
+
+        let spotPreviewImage = ''
+        if (spotImages.length) {
+            spotImages.forEach(spotImage => {
+                if (spotImage.preview === true) {
+                    spotPreviewImage = spotImage.url
+                }
+            })
+        }
+
+      
+        spotJson.previewImage = spotPreviewImage || 'No preview image yet'
+        
+
+        listOfBookings.push(booking)
+    }
+
+    
+    if (!listOfBookings.length) listOfBookings = 'No bookings yet'
+    res.status(200).json({ Bookings: listOfBookings })
+})
+
+
+
+
+
+
+
+module.exports = router;
