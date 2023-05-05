2b4c8ed2462c0fa9c8369f66eea767240b3e2307
Carmen Lee <carmenlee2278@gmail.com>
2023-05-04 22:38:58 -0700

    restore session user thunk made

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,16 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
@@ -47,5 +57,9 @@ const sessionReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+
+
+
 
 export default sessionReducer;
