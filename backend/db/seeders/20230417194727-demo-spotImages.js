75af65b8136cb45a1c4e3d4e497f5b543ee4e00b
Carmen Lee <carmenlee2278@gmail.com>
2023-05-09 13:05:42 -0700

    fixed some seeder data

--- backend/db/seeders/20230417194727-demo-spotImages.js
+++ backend/db/seeders/20230417194727-demo-spotImages.js
@@ -12,40 +12,54 @@ module.exports = {
     await queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        url: 'url preview',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-668146487515150072/original/8ff2a532-e0cd-41a2-9164-554c4d9eb28a.jpeg',
         preview: true
       },
       {
         spotId: 1,
-        url: 'url non-preview 1',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-668146487515150072/original/7da9fd6a-8d6c-4954-88b0-1fc52abc9cfd.jpeg',
         preview: false
       },
       {
         spotId: 1,
-        url: 'url non-preview 2',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-668146487515150072/original/41cbfe4c-56b1-4a67-8365-47a5894816a4.jpeg',
         preview: false
       },
       {
         spotId: 1,
-        url: 'url non-preview 3',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-668146487515150072/original/5bf7252a-1b94-4a85-aa3b-581c16d6bd69.jpeg',
         preview: false
       },
       {
         spotId: 1,
-        url: 'url non-preview 4',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-668146487515150072/original/5bf7252a-1b94-4a85-aa3b-581c16d6bd69.jpeg',
         preview: false
       },
       {
         spotId: 2,
-        url: 'url preview',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-23206143/original/e7da1f36-922f-4631-a287-91ceda05970f.jpeg',
         preview: true
       },
       {
-        spotId: 3,
-        url: 'url preview',
-        preview: true
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-23206143/original/e80ace7e-c494-49b9-9595-08ab76ef4365.jpeg',
+        preview: false
       },
-  
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-23206143/original/55b0263c-756d-466b-b050-39b031398a1d.jpeg',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-23206143/original/b779ec9c-080a-4788-9391-512f7d247aa0.jpeg',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-23206143/original/60b99416-8264-4a29-b486-cd6721117423.jpeg',
+        preview: false
+      }
     ])
   },
 
