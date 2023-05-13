0a859df4634554ee457a8d356129dc5dde8e4595
Carmen Lee <carmenlee2278@gmail.com>
2023-05-12 20:07:14 -0700

    added button disability

--- frontend/src/components/ReviewModal/ReviewModal.js
+++ frontend/src/components/ReviewModal/ReviewModal.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useDispatch } from "react-redux";
 import { useModal } from '../../context/Modal'
-import {createReviewThunk} from '../../store/reviews'
+import {createReviewThunk, reviewsReducer} from '../../store/reviews'
 import { getReviewsBySpotThunk } from '../../store/reviews';
 
 function ReviewModal({spotId}) {
@@ -10,6 +10,7 @@ function ReviewModal({spotId}) {
 
     const [review, setReview] = useState('')
     const [stars, setStars] = useState(1)
+    const [errors, setErrors] = useState('')
 
     const newReview = {
         review,
@@ -21,11 +22,15 @@ function ReviewModal({spotId}) {
         dispatch(getReviewsBySpotThunk(spotId))  
     }, [dispatch])
 
+    useEffect(() => {
+        const errorsDisplayed = {}
+        if (review.length >= 1 && review.length < 10) errorsDisplayed.review = "Please write a review of more than 10 characters"
+        setErrors(errorsDisplayed)
+    }, [review])
+
     const handleSubmit = () => {
-       
         dispatch(createReviewThunk(newReview))
         closeModal()
- 
     }
 
 
@@ -34,6 +39,7 @@ function ReviewModal({spotId}) {
     return (
         <>
             <h1>How was your stay?</h1>
+            {errors && errors.review}
             <textarea
             placeholder='Leave your review here...'
             onChange={(e) => setReview(e.target.value)}
@@ -52,7 +58,7 @@ function ReviewModal({spotId}) {
 
             {/* {console.log('stars', stars)} */}
 
-            <button onClick={handleSubmit}>{`Submit Your Review`}</button>
+            <button onClick={handleSubmit} disabled={Object.values(errors).length > 0}>{`Submit Your Review`}</button>
           
         </>
     )
