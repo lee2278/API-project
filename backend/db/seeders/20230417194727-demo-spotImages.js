39a57e19cf6a5aff7dcf9884c09f3fa37d9b65bd
Carmen Lee <carmenlee2278@gmail.com>
2023-05-12 23:31:20 -0700

    fixed some seeder data

--- backend/db/seeders/20230417194727-demo-spotImages.js
+++ backend/db/seeders/20230417194727-demo-spotImages.js
@@ -59,7 +59,57 @@ module.exports = {
         spotId: 2,
         url: 'https://a0.muscache.com/im/pictures/miso/Hosting-23206143/original/60b99416-8264-4a29-b486-cd6721117423.jpeg',
         preview: false
-      }
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-13016398/original/6c59e24d-89f3-4475-aaca-80363792fb51.jpeg',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-13016398/original/7629c923-3741-4ee8-9520-4685578aad4a.jpeg',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-13016398/original/2161c70b-83ac-4ef8-83fd-8ff358fcbe2b.jpeg',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-13016398/original/1e16174c-e9a6-4f2a-b20d-43e049cdd85d.jpeg',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-13016398/original/60d17abb-e93a-446b-b3a1-740f0773d163.jpeg',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-635437605163910390/original/eaf8887f-410f-41e4-be1b-88c2a74fbfcf.jpeg',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-635437605163910390/original/427b9c9e-27a1-45ce-9f04-0609eb74cb48.jpeg',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-635437605163910390/original/0db5cffd-7f0d-4e13-bd59-39cb9861cdaa.jpeg',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-635437605163910390/original/5a6ee29b-2c64-440a-aee6-481d4d8d1bd5.jpeg',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-635437605163910390/original/e22d9d0e-fa0b-48e1-9d35-ab7c3463b357.jpeg',
+        preview: false
+      },
     ])
   },
 
--- backend/db/seeders/20230417195754-demo-reviews.js
+++ backend/db/seeders/20230417195754-demo-reviews.js
@@ -13,31 +13,31 @@ module.exports = {
    await queryInterface.bulkInsert(options, [
     {
       spotId: 1,
-      userId: 1,
+      userId: 2,
       review: 'Very clean, nice amenities.',
       stars: 5,
     },
     {
       spotId: 1,
-      userId: 2,
+      userId: 3,
       review: 'Stay was pleasant.',
       stars: 4
     },
     {
       spotId:2,
-      userId: 1,
+      userId: 2,
       review: 'Location was great. Close to tourist attractions.',
       stars: 5
     },
     {
       spotId:2,
       userId: 3,
-      review: 'Stay was ok.',
+      review: 'Stay was ok. Not bad',
       stars: 3
     },
     {
       spotId:3,
-      userId: 2,
+      userId: 1,
       review: 'Had a good time. Everything well provided.',
       stars: 5
     }
