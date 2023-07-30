9ba81cd86838ebed6950bf746a848e76b51b55c0
Carmen Lee <carmenlee2278@gmail.com>
2023-07-29 21:43:27 -0700

    added in github linkedin links

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
@@ -12,18 +12,27 @@ function Navigation({ isLoaded }){
       <li className='home'>
         <NavLink id='home-link' exact to="/">{
           <>
-         <img id='logo' src="https://happybnb.s3.us-west-1.amazonaws.com/1690525771935.png" alt='pikachu'/>
-          <p>Happybnb</p>
+            <img id='logo' src="https://happybnb.s3.us-west-1.amazonaws.com/1690525771935.png" alt='pikachu' />
+            <p>Happybnb</p>
           </>}
-          </NavLink>
+        </NavLink>
+        <div className='my-links-section'>Site created by: Carmen Lee
+          
+          <a id='github-atag' href="https://github.com/lee2278" target="_blank">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a id='linkedin-atag' href="https://www.linkedin.com/in/carmen-lee-52061690/" target="_blank">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
       </li>
       <div className='group-right'>
-      {sessionUser && (<li><NavLink id= 'create-spot-navlink' exact to='/spots/new'>Create a New Spot</NavLink></li>)}
-      {isLoaded && (
-        <li className='profile-btn'>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
+        {sessionUser && (<li><NavLink id='create-spot-navlink' exact to='/spots/new'>Create a New Spot</NavLink></li>)}
+        {isLoaded && (
+          <li className='profile-btn'>
+            <ProfileButton user={sessionUser} />
+          </li>
+        )}
       </div>
     </div>
   );
